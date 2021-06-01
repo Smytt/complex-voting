0491eaa557ecd2560de49b7cde4d369325f1478e
Kiril Parushev <kiril.parushev@tick42.com>
2021-06-01 21:59:53 +0300

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import firebase from 'firebase'
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  // const [user, setUser] = useState<firebase.User | null>(null);
+  var provider = new firebase.auth.EmailAuthProvider();
+  firebase.auth().signInWithPopup(provider)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <p>Hi!</p>
+  )
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,19 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import firebase from 'firebase/app';
+
+const config = {
+  apiKey: "AIzaSyBg49A4-rwUOkdAWEIv7k_4h6k4U3GTvxg",
+  authDomain: "complex-voting.firebaseapp.com",
+  projectId: "complex-voting",
+  storageBucket: "complex-voting.appspot.com",
+  messagingSenderId: "129002144891",
+  appId: "1:129002144891:web:50972f69a2954940b18ede"
+};
+
+firebase.initializeApp(config)
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
